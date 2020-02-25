3f8d98d5883c7214d3e11708b8a4be9b820b4fbf
Joshua Bence <jbence@netflix.com>
2020-02-24 21:37:29 -0800

    feat: add right sidebar
    - move game code to separate
    - add right sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,16 @@
 import React from "react";
-import { useGameState, playerSelectIndex } from "./hooks/useGameState";
+import { useGameState } from "./hooks/useGameState";
 import { CommandPanel } from "./components/CommandPanel";
-import Chip from "@material-ui/core/Chip";
-import Grid from "@material-ui/core/Grid";
-import TagFacesIcon from "@material-ui/icons/TagFaces";
-import Typography from "@material-ui/core/Typography";
-import { red, blue } from "@material-ui/core/colors";
-import { GRID_CELL_WIDTH, GRID_WIDTH } from "./contants/GameSettings";
-import { GridBoard } from "./components/GridBoard";
-
-import { pointToIndex } from "./utils/GridUtil";
-
-import "./App.css";
-
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { GameStatus } from "./models/Game";
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    gridBoard: {
-      flex: 1
-    },
-    gridApp: {
-      height: "100%"
-    },
-    gridTeams: {
-      padding: 10,
-      minHeight: 52
-    }
-  })
-);
+import { AppLayout } from "./components/AppLayout";
+import { TicTacToe } from "./components/games/TicTacToe";
 
 export default function App() {
-  const classes = useStyles();
   const [state, dispatch] = useGameState();
 
-  const handleBoxClick = (e: React.MouseEvent) => {
-    e.persist();
-    const x = Math.floor(e.nativeEvent.offsetX / GRID_CELL_WIDTH);
-    const y = Math.floor(e.nativeEvent.offsetY / GRID_CELL_WIDTH);
-    dispatch(playerSelectIndex(pointToIndex({ x, y })));
-  };
-
   return (
-    <div className="App">
+    <AppLayout>
       <CommandPanel dispatch={dispatch} state={state} />
-      <Grid container direction="column" className={classes.gridApp}>
-        <Grid
-          container
-          direction="row"
-          justify="space-evenly"
-          alignItems="center"
-          className={classes.gridTeams}
-        >
-          {state.teams.map(ts => (
-            <Grid item key={ts.team}>
-              <Chip
-                avatar={
-                  <TagFacesIcon
-                    style={{ color: ts.team === 0 ? red[500] : blue[500] }}
-                  />
-                }
-                label={`${ts.name} Score: ${ts.score}`}
-              />
-            </Grid>
-          ))}
-        </Grid>
-        <Grid container direction="row" justify="center" alignItems="center">
-          <Typography variant="h5" gutterBottom>
-            {state.gameStatus === GameStatus.PLAYING
-              ? `${state.teams[state.currentTeam].name}'s Turn`
-              : state.gameStatus === GameStatus.WIN
-              ? `${state.teams[state.currentTeam].name} Wins`
-              : state.gameStatus}
-          </Typography>
-        </Grid>
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          className={classes.gridBoard}
-        >
-          <GridBoard
-            grid={state.grid}
-            width={GRID_WIDTH}
-            height={GRID_WIDTH}
-            onClick={handleBoxClick}
-          />
-        </Grid>
-      </Grid>
-    </div>
+      <TicTacToe dispatch={dispatch} state={state} />
+    </AppLayout>
   );
 }
---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,75 @@
+import React, { FunctionComponent } from "react";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
+import FolderOutlinedIcon from "@material-ui/icons/FolderOutlined";
+
+const useStyles = makeStyles((theme: Theme) => {
+  const drawerWidth = theme.spacing(7) + 1;
+  return createStyles({
+    root: {
+      display: "flex"
+    },
+    appBar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginRight: drawerWidth
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0
+    },
+    drawerPaper: {
+      width: drawerWidth,
+      overflowX: "hidden"
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.default,
+      position: "relative"
+    }
+  });
+});
+
+interface AppLayoutProps {
+  children: any;
+}
+
+export const AppLayout: FunctionComponent<AppLayoutProps> = ({ children }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <main className={classes.content}>{children}</main>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper
+        }}
+        anchor="right"
+      >
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <PersonOutlineOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Account"} />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <FolderOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Library"} />
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
+  );
+};
